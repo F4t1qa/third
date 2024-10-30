b805d8329b9498c778e4dcb096bd1355e06e6fa2
fan <65284460@qq.com>
2024-10-30 22:01:47 +0800

    1

--- auto.bat
+++ auto.bat
@@ -3,11 +3,13 @@ call npm run build
 @echo off
 set folderPath=C:\Users\Administrator\Desktop\second
 
-:: 删除文件夹中的文件
-del /S /Q %folderPath%\*.*
+set "folderPath=C:\Users\Administrator\Desktop\second"
 
-:: 删除文件夹中的子文件夹及其内容
-for /F %%i in ('dir /B %folderPath%') do rmdir /S /Q %folderPath%\%%i
+for /F %%i in ('dir /B "%folderPath%"') do (
+    if not "%%i"=="auto.bat" (
+        rmdir /S /Q "%folderPath%\%%i"
+    )
+)
 
 :: 剪切 dist 文件夹中的内容到目标文件夹
 xcopy /E /Y "dist\*" "%folderPath%"
@@ -15,7 +17,5 @@ xcopy /E /Y "dist\*" "%folderPath%"
 :: 删除原始 dist 文件夹
 rmdir /S /Q dist
 
-cd folderPath
-git add .
-git commit -m '1'
-git push
+
+C:\Users\Administrator\Desktop\second\auto.bat 
